37c5db24f04552943cf41334065103bd9070a0fc
unknown <doris.bednarska@gmail.com>
2017-02-15 19:43:02 +0100

    Added basic functionality in MyList class

--- src/algorytmy/Application.java
+++ src/algorytmy/Application.java
@@ -1,10 +1,25 @@
 package algorytmy;
 
+import algorytmy.structures.MyList;
+
 /**
  * Created by RENT on 2017-02-14.
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        MyList myList = new MyList();
+        myList.add(2);
+        myList.add(-9);
+        myList.add(11);
+        myList.add(7);
+        myList.add(3);
+        myList.add(0);
+        myList.add(-1);
+        myList.add(56);
+        myList.add(48);
+        myList.add(4);
+        myList.add(5);
+        MyList clonedList = myList.clone();
+        clonedList.add(5);
     }
 }
